327c28bf8d87e658f42c429c529e48fba0fd476e
xuliangmost <xuliang.most@gmail.com>
2016-12-06 21:29:05 +0800

    Tb

---
+++ Tb/js/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2016/12/6.
+ */
+
+var mySwiper = new Swiper('.swiper-container', {
+    autoplay: 2000,
+    pagination : '.swiper-pagination',
+    autoplayDisableOnInteraction : false
+})
