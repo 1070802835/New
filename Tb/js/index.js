ccfeb6df32e85f522385c0b288f0d7b238c32679
xuliangmost <xuliang.most@gmail.com>
2016-12-07 21:45:34 +0800

    12-7-night

--- Tb/js/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Administrator on 2016/12/6.
- */
-
-var mySwiper = new Swiper('.swiper-container', {
-    autoplay: 2000,
-    pagination : '.swiper-pagination',
-    autoplayDisableOnInteraction : false
-})
